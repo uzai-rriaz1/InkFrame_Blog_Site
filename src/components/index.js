738ab05512dab8665cc32a2ecc4cf076297e3153
uzai-rriaz1 <uzairriaz1296@gmail.com>
2025-08-27 20:47:07 +0500

    InkFrame Blog app using appwrite service as backend

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/container.jsx";
+import Logo from "./logo";
+import Logout_Btn from "./Header/logout_btn.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
+import Select from "./Select.jsx";
+import RTE from "./RTE.jsx";
+import Login from "./Login.jsx";
+import SignUp from "./SignUp.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Logout_Btn,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Login,
+  SignUp,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
